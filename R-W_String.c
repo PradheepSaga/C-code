a0744ce577af661841498a19355c8d484d6045e5
PradheepSaga <148743074+PradheepSaga@users.noreply.github.com>
2024-09-21 16:26:55 +0530

    Add files via upload

---
+++ R-W_String.c
@@ -0,0 +1,13 @@
+/* Read and write the string in C
+   written by K.Pradheepkuar dated 21-09-2024 */
+
+#include<stdio.h>
+
+int main(){
+
+	char s[20];        // Declaration of string
+	printf("Enter the string: ");
+	gets(s);           // Similar to scanf
+	puts(s);          // Similar to printf
+	return 0;
+	}
