fc97b8f89f9e1098171c91ff626643cfae96e3f0
PradheepSaga <148743074+PradheepSaga@users.noreply.github.com>
2024-09-21 16:07:42 +0530

    Add files via upload

---
+++ String.c
@@ -0,0 +1,13 @@
+/* String in C
+   written by K.Pradheepkumar dated 21-09-2024 */
+
+#include<stdio.h>
+
+int main(){
+
+	char s[7] = {'S','t','r','i','n','g','\0'}; // Declaration and initialization of string
+
+		printf("%s", s);  // Print the array of characters 
+		
+	return 0;
+	}
